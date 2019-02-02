d681edca499105a2e4c9cf29517863001cf0dc54
Salvatore Novelli <salvatore.novelli@gmail.com>
2019-02-02 12:18:49 +0000

    Releasing v2.0.0 Adding second microservice + Cloud build configuration

--- Server.js
+++ Server.js
@@ -4,48 +4,59 @@ let httpProxy = require('http-proxy');
 
 let apiProxy = httpProxy.createProxyServer();
 let frontend = 'http://frontend-service:5000',
+    archive = 'http://archive:8080',
     backend = 'http://backend-service:8080';
-
 
 
 const dns = require('dns');
 
 dns.lookup('frontend-service', (err, address, family) => {
-    console.log('frontend-service: %j family: IPv%s', address, family);
+    console.info('frontend-service: %j family: IPv%s', address, family);
 });
 
 dns.lookup('backend-service', (err, address, family) => {
-    console.log('backend-service: %j family: IPv%s', address, family);
+    console.info('backend-service: %j family: IPv%s', address, family);
 });
 
-
+dns.lookup('archive', (err, address, family) => {
+    console.info('archive: %j family: IPv%s', address, family);
+});
 
 let port = 80;
 
 
 process.on('uncaughtException', function (err) {
-    console.log("Uncaught exception: " + err.message);
+    console.error("Uncaught exception: " + err.message);
 });
 
 app.all("/health", function (req, res) {
-    console.log('Health check invoked');
+    console.info('Health check invoked');
     res.send('I\'m fine!')
 });
 
+
 app.all("/api/*", function (req, res) {
-    console.log('redirecting ' + req.originalUrl + ' to backend');
-    apiProxy.web(req, res, {target: backend});
+
+    let dst = backend;
+
+    let tokens = req.originalUrl.split("/");
+    if (tokens[2] === "archive-api") {
+        dst = archive;
+    }
+
+    console.info('redirecting ' + req.originalUrl + " to " + dst);
+    apiProxy.web(req, res, {target: dst});
 });
 
 app.ws('*', function (ws, req) {
     ws.on('*', function (msg) {
-        console.log(msg);
+        console.info(msg);
     });
-    console.log('Websocket request intercepted');
+    console.info('Websocket request intercepted');
 });
 
 app.all("*", function (req, res) {
-    console.log('redirecting ' + req.originalUrl + ' to frontend');
+    console.info('redirecting ' + req.originalUrl + ' to frontend');
     apiProxy.web(req, res, {target: frontend});
 });
 
--- build_helper.sh
+++ build_helper.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 
 
 export GCE_PROJECT_ID="$(gcloud config get-value project -q)"
@@ -8,21 +9,73 @@ export ARTIFACT_ID=`jq -r '.name' package.json`
 export IMAGE_TAG=gcr.io/${GCE_PROJECT_ID}/${ARTIFACT_ID}:${VERSION}
 
 
-if [ -z "$1" ]
+if [[ -z "$1" ]]
   then
     echo "No argument supplied"
     exit
 fi
 
+echo "Going to $1 ${IMAGE_TAG}"
+
+confirm() {
+    read -r -p "Do you want to continue? [y/N]" response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NO_COLOR='\033[0m'
 
 case $1 in
+    "cloud-build" )
+
+
+        GIT_TAG_NAME=$(git tag -l --points-at HEAD)
+        GIT_SHORT_SHA=$(git rev-parse --short HEAD)
+
+        if [[ -z ${GIT_TAG_NAME} ]]
+          then
+            GIT_TAG_NAME="untagged"
+        fi
+
+        LOCAL_ID=$(whoami)_$(hostname)
+
+        TAG_NAME=${LOCAL_ID}"."${GIT_TAG_NAME}
+        SHORT_SHA=${LOCAL_ID}"."${GIT_SHORT_SHA}
+
+        echo -e "Triggering remote build for:"
+        echo -e " TAG: ${BLUE}${TAG_NAME}${NO_COLOR}"
+        echo -e " SHA: ${BLUE}${SHORT_SHA}${NO_COLOR}"
+        ! confirm  && exit
+
+
+        SUBST=TAG_NAME=${TAG_NAME},SHORT_SHA=${SHORT_SHA}
+        gcloud builds submit --substitutions=${SUBST} --machine-type=n1-highcpu-8
+#       cloud-build-local --substitutions=${SUBST} --dryrun=false --write-workspace=../tmp_cloudbuildlocal .
+    ;;
     "build" )
+        yarn install || exit 1
+        yarn build || exit 1
+        rm -rf docker/build
+        mv build docker
         echo "Building ${IMAGE_TAG}"
-        npm install
-        docker build -t ${IMAGE_TAG} .
+        docker build docker -t ${IMAGE_TAG}
+    ;;
+    "clean" )
+        rm -rf build
+        rm -rf docker/build
     ;;
     "run" )
-        docker run -it --rm -p 5000:80 ${IMAGE_TAG}
+        docker run --name ${ARTIFACT_ID} -it --rm -p 3001:3001 ${IMAGE_TAG}
     ;;
     "push" )
         gcloud docker -- push ${IMAGE_TAG}
@@ -30,12 +83,12 @@ case $1 in
     "deploy" )
         kubectl delete -f k8s/production.yaml
         sed -i.bak "s#<IMAGE_TAG_DO_NOT_EDIT>#${IMAGE_TAG}#" k8s/production.yaml
-        kubectl apply -f k8s/production.yaml
+        kubectl apply -f k8s/
         mv k8s/production.yaml.bak k8s/production.yaml
     ;;
+    *)
+        echo "'$1' is not a valid action"
+    ;;
 esac
-
-
-
 
 
